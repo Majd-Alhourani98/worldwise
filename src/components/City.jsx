1bb8e9e21962d6dbbb15a5dbca379b672dd637bc
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-11-11 23:59:39 +0300

    Implemented City component to display detailed information for a single city using useParams and getCity from useCities.

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,12 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+import { useCities } from "../../contexts/CitiesContext";
+
+import Spinner from "./Spinner";
+import Message from "./Message";
+
 import styles from "./City.module.css";
+import { useEffect } from "react";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -11,16 +18,49 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-  const [searchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const { getCity, currentCity, isLoading } = useCities();
+  const { cityName, emoji, date, notes } = currentCity;
+
+  useEffect(
+    function () {
+      getCity(id);
+    },
+    [id]
+  );
+
+  if (isLoading) return <Spinner />;
 
   return (
     <div className={styles.city}>
-      <h1>City {id}</h1>
-      <p>
-        Position: {lng}, {lat}
-      </p>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
+
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
+
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia →
+        </a>
+      </div>
     </div>
   );
 }
